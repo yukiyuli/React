3544282216ae96b8928430874351cf566fd1d598
Yuki Yu <101588752+yukiyuli@users.noreply.github.com>
2022-11-29 08:59:47 -0500

    react todo list demo

---
+++ todolist/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @作者: kerwin
+ * @公众号: 大前端私房菜
+ */
+// console.log("hello world kerwin")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './09-immutable/redux/App'
+
+
+ReactDOM.render(
+    // <React.StrictMode>
+    <App/>
+    // </React.StrictMode>
+    ,document.getElementById("root"))
