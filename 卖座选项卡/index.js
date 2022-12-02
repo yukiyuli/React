ca0666363e176f88f909c97807cda8b64fdf90bc
Yuki Yu <101588752+yukiyuli@users.noreply.github.com>
2022-12-01 20:08:54 -0500

    Add files via upload

---
+++ 卖座选项卡/index.js
@@ -0,0 +1,16 @@
+/*
+ * @作者: kerwin
+ * @公众号: 大前端私房菜
+ */
+// console.log("hello world kerwin")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './12-卖座选项卡.js'
+
+
+ReactDOM.render(
+    // <React.StrictMode>
+    <App/>
+    // </React.StrictMode>
+    ,document.getElementById("root"))
