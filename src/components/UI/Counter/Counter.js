87450a6b9bba1dd141e1f71abee687284c2c0a78
Yuki Yu <101588752+yukiyuli@users.noreply.github.com>
2023-01-30 23:06:12 -0500

    meal delivery react exercie

---
+++ src/components/UI/Counter/Counter.js
@@ -0,0 +1,70 @@
+import React, { useContext } from 'react'
+import classes from './Counter.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import CartContext from '../../../store/cart-context'
+
+
+/* 
+
+- import FontAwesome
+  1. Add SVG Core
+      npm i --save @fortawesome/fontawesome-svg-core
+  2. Add Icon Packages
+      # Free icons styles
+      npm i --save @fortawesome/free-solid-svg-icons
+      npm i --save @fortawesome/free-regular-svg-icons
+  3. Add the React Component
+      npm i --save @fortawesome/react-fontawesome@latest
+
+- import component
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+- import icon
+  import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+*/
+
+
+// counter component
+const Counter = (props) => {
+
+  const ctx = useContext(CartContext);
+
+  // function of adding cart
+  const addButtonHandler = () => {
+    ctx.addItem(props.meal);
+  };
+
+  // function of deleting food
+  const subButtonHandler = () => {
+    ctx.removeItem(props.meal);
+  };
+
+  return (
+    <div className={classes.Counter}>
+
+      {/* if amount = 0, don't show number 0
+             use Fragment to make button "-" and amount in one block
+          
+        */}
+      {
+        (props.meal.amount && props.meal.amount !== 0) ? (
+          <>
+            <button
+              onClick={subButtonHandler}
+              className={classes.Sub}><span><FontAwesomeIcon icon={faMinus} /></span></button>
+            <span className={classes.Count}>{props.meal.amount}</span>
+          </>
+        ) : null
+      }
+
+
+      <button
+        onClick={addButtonHandler}
+        className={classes.Add}><FontAwesomeIcon icon={faPlus} /></button>
+    </div>
+  )
+}
+
+export default Counter
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+
+// Set the adaptation of the mobile terminal
+// the value rem is divided by the width of the viewport, now we set the total width of the viewport to 750rem
+document.documentElement.style.fontSize = 100 / 750 + 'vw';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
