34e19067af0f94a97c3ed20b3dc2ca8699eaf2c1
yukiyuli <yukiyuli6688@gmail.com>
2023-03-09 19:30:32 -0500

    add diy hooks

--- React project/project/student_1/src/App.js
+++ React project/project/student_1/src/App.js
@@ -2,37 +2,13 @@ import React, { useCallback, useEffect, useState } from 'react';
 import StudentList from "./components/StudentList";
 import './App.css';
 import StuContext from "./store/StuContext";
+import useFetch from './hooks/usefetch';
 
 
 const App = () => {
 
-    const [stuData, setStuData] = useState([]);
+    const { data: stuData, loading, error, fetchData } = useFetch();
 
-    // 添加一个state来记录数据是否正在加载,false表示没有加载数据，true表示加载
-    const [loading, setLoading] = useState(false);
-
-    // 创建一个state来记录错误信息
-    const [error, setError] = useState(null);
-
-    const fetchData = useCallback(async () => {
-        try {
-            setLoading(true);
-            setError(null);
-            const res = await fetch('http://localhost:1337/api/students');
-            //判断请求是否加载成功
-            if (res.ok) {
-                const data = await res.json();
-                setStuData(data.data);
-            } else {
-                throw new Error('Loading failed!');
-            }
-        } catch (e) {
-            setError(e);
-        } finally {
-            setLoading(false);
-        }
-
-    },[]);
 
     // 组件重新加载
     useEffect(() => {
@@ -40,18 +16,18 @@ const App = () => {
     }, []);
 
     // 点击按钮
-    const loadDataHandler=()=>{
+    const loadDataHandler = () => {
         fetchData();
     };
 
     return (
-        <StuContext.Provider value={{fetchData}}>
-        <div className="app">
-            <button onClick={loadDataHandler}>Load data</button>
-            {(!loading && !error) && <StudentList stus={stuData} />}
-            {loading && <p>Loading...</p>}
-            {error && <p>Loading error！</p>}
-        </div>
+        <StuContext.Provider value={{ fetchData }}>
+            <div className="app">
+                <button onClick={loadDataHandler}>Load data</button>
+                {(!loading && !error) && <StudentList stus={stuData} />}
+                {loading && <p>Loading...</p>}
+                {error && <p>Loading error！</p>}
+            </div>
         </StuContext.Provider>
     );
 };
---
+++ React project/project/student_1/src/hooks/usefetch.js
@@ -0,0 +1,38 @@
+import { useCallback, useState } from 'react';
+
+export default function useFetch() {
+
+    const [data, setData] = useState([]);
+
+    // 添加一个state来记录数据是否正在加载,false表示没有加载数据，true表示加载
+    const [loading, setLoading] = useState(false);
+
+    // 创建一个state来记录错误信息
+    const [error, setError] = useState(null);
+
+    const fetchData = useCallback(async () => {
+        try {
+            setLoading(true);
+            setError(null);
+            const res = await fetch('http://localhost:1337/api/students');
+            //判断请求是否加载成功
+            if (res.ok) {
+                const data = await res.json();
+                setData(data.data);
+            } else {
+                throw new Error('Loading failed!');
+            }
+        } catch (e) {
+            setError(e);
+        } finally {
+            setLoading(false);
+        };
+
+    }, []);
+
+    // set return value
+    return {
+        loading, error, data, fetchData
+    };
+
+}
